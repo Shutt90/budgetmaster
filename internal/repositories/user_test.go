b64e2624f1676cab209e05aff50747eb070ffd36
Liam Pugh <liam.pugh.009@gmail.com>
2024-04-02 19:33:19 +0100

    test for get password added

--- internal/repositories/user_test.go
+++ internal/repositories/user_test.go
@@ -1,6 +1,7 @@
 package repositories
 
 import (
+	"database/sql"
 	"encoding/json"
 	"reflect"
 	"regexp"
@@ -32,6 +33,13 @@ func TestGetByEmail(t *testing.T) {
 			expectedErr:    nil,
 			expectedResult: `{"ID":1,"FirstName":"fname","Surname":"surname","Email":"test@example.com","Password":"password","CreatedAt":{"Time":"0001-01-01T00:00:00Z","Valid":false},"UpdatedAt":{"Time":"0001-01-01T00:00:00Z","Valid":false}}`,
 		},
+		{
+			name:           "get user not found",
+			email:          "test2@example.com",
+			ur:             mockRepo,
+			expectedErr:    ErrNotFound,
+			expectedResult: "null",
+		},
 	}
 
 	userMockRows := sqlmock.NewRows([]string{"id", "firstName", "surname", "password"}).
@@ -42,6 +50,12 @@ func TestGetByEmail(t *testing.T) {
 		WithArgs(
 			"test@example.com",
 		).WillReturnRows(userMockRows)
+
+	mock.ExpectQuery(
+		regexp.QuoteMeta(`SELECT id, firstName, surname, password FROM user WHERE email = ?;`)).
+		WithArgs(
+			"test2@example.com",
+		).WillReturnError(sql.ErrNoRows)
 
 	for _, tc := range testcases {
 		t.Run(tc.name, func(t *testing.T) {
@@ -59,6 +73,46 @@ func TestGetByEmail(t *testing.T) {
 				if tc.expectedResult != string(iBytes) {
 					t.Errorf("unexpected result \n want %s\nhave: %s\n", tc.expectedResult, string(iBytes))
 				}
+			}
+		})
+	}
+}
+
+func TestChangePassword(t *testing.T) {
+	type testcase struct {
+		name           string
+		email          string
+		password       string
+		ur             ports.UserRepository
+		expectedErr    error
+		expectedResult string
+	}
+
+	db, mock, _ := sqlmock.New()
+	defer db.Close()
+	mockRepo := NewUserRepository(db)
+
+	testcases := []testcase{
+		{
+			name:        "get user success",
+			email:       "test@example.com",
+			password:    "password",
+			ur:          mockRepo,
+			expectedErr: nil,
+		},
+	}
+
+	mock.ExpectExec(
+		regexp.QuoteMeta(`UPDATE user SET password TO ? WHERE email = ?;`)).
+		WithArgs(
+			"password",
+		).WillReturnResult(sqlmock.NewResult(1, 1))
+
+	for _, tc := range testcases {
+		t.Run(tc.name, func(t *testing.T) {
+			err := tc.ur.ChangePassword(tc.email, tc.password)
+			if err != tc.expectedErr {
+				t.Errorf("unexpected error\n want: %s\nhave: %s\n", tc.expectedErr, err.Error())
 			}
 		})
 	}
