cc39133a3880fa2bc4072f92a9caacbd5f7864cc
Liam Pugh <liam.pugh.009@gmail.com>
2024-04-02 13:18:29 +0100

    added more checks and tests for new user

--- internal/core/domain/item_test.go
+++ internal/core/domain/item_test.go
@@ -4,13 +4,13 @@ import (
 	"testing"
 )
 
-func TestChangedRecurring(t *testing.T) {
+func TestNewItem(t *testing.T) {
 	t.Run("checks item created", func(t *testing.T) {
 		i := NewItem(
-			"testItem",
-			"testDescriptions",
-			"testLocation",
-			"testMonth",
+			"name",
+			"description",
+			"location",
+			"month",
 			2024,
 			100,
 			true,
@@ -19,5 +19,34 @@ func TestChangedRecurring(t *testing.T) {
 		if i == nil {
 			t.Error("new item not created")
 		}
+
+		if i.Name != "name" {
+			t.Error("incorrect item name")
+		}
+
+		if i.Description != "description" {
+			t.Error("incorrect item description")
+		}
+
+		if i.Location != "location" {
+			t.Error("incorrect location")
+		}
+
+		if i.Month != "month" {
+			t.Error("incorrect month")
+		}
+
+		if i.Year != 2024 {
+			t.Error("incorrect year")
+		}
+
+		if i.Cost != 100 {
+			t.Error("incorrect item cost")
+		}
+
+		if i.IsRecurring != true {
+			t.Error("incorrect recurring")
+		}
+
 	})
 }
