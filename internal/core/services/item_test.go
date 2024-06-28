84203a2455c97a77300f633a0447792a15d4210f
Liam Pugh <liam.pugh.009@gmail.com>
2024-06-28 21:23:28 +0100

    add template for get monthly defaults

--- internal/core/services/item_test.go
+++ internal/core/services/item_test.go
@@ -1,6 +1,8 @@
 package services
 
 import (
+	"database/sql"
+	"encoding/json"
 	"fmt"
 	"regexp"
 	"testing"
@@ -8,6 +10,7 @@ import (
 	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/Shutt90/budgetmaster/internal/core/domain"
 	"github.com/Shutt90/budgetmaster/internal/core/ports"
+	"github.com/Shutt90/budgetmaster/internal/core/services"
 	"github.com/Shutt90/budgetmaster/internal/repositories"
 )
 
@@ -24,7 +27,8 @@ func TestCreate(t *testing.T) {
 
 	mockClock := NewMockClock()
 
-	mockService := NewItemService(repositories.NewItemRepository(db, NewMockClock()), mockClock)
+	// remove mock clocks into single struct
+	mockService := NewItemService(repositories.NewItemRepository(db, mockClock), mockClock)
 
 	testcases := []testcase{
 		{
@@ -57,6 +61,62 @@ func TestCreate(t *testing.T) {
 		t.Run(tc.name, func(t *testing.T) {
 			if err := tc.ir.Create(tc.item()); err != nil {
 				t.Errorf("err when adding new item, %s", err.Error())
+			}
+		})
+	}
+}
+
+func TestGetDefaultMonthlyItems(t *testing.T) {
+	type testcase struct {
+		name           string
+		month          string
+		year           uint16
+		ir             ports.ItemRepository
+		expectedErr    error
+		expectedResult string
+	}
+
+	db, mock, _ := sqlmock.New()
+	defer db.Close()
+	mockClock := services.NewMockClock()
+
+	// remove mock clocks into single struct
+	mockService := NewItemService(repositories.NewItemRepository(db, mockClock), mockClock)
+
+	testcases := []testcase{
+		{
+			name:           "get monthly for period of jan",
+			ir:             mockService.itemRepository,
+			expectedErr:    nil,
+			expectedResult: `[{"id":1,"name":"testName","description":"testDesc","location":"testLoc","cost":100,"isRecurring":true,"createdAt":{"Time":"2024-01-27T16:26:00Z","Valid":true},"updatedAt":{"Time":"2024-03-27T16:26:00Z","Valid":true}},{"id":2,"name":"testName2","description":"testDesc2","location":"testLoc2","cost":200,"isRecurring":false,"createdAt":{"Time":"2024-01-27T16:26:00Z","Valid":true},"updatedAt":{"Time":"2024-03-27T16:26:00Z","Valid":true}}]`,
+		},
+	}
+
+	itemMockRows := sqlmock.NewRows([]string{"id", "name", "description", "location", "cost", "isRecurring", "removedRecurringAt", "createdAt", "updatedAt"}).
+		AddRow(1, "testName", "testDesc", "testLoc", 100, "1", sql.NullTime{}, mockService.clock.Jan(), mockService.clock.Now()).
+		AddRow(2, "testName2", "testDesc2", "testLoc2", 200, "0", sql.NullTime{}, mockService.clock.Jan(), mockService.clock.Now())
+
+	mock.ExpectQuery(
+		regexp.QuoteMeta(`SELECT * FROM item WHERE strftime('%m', createdAt) = ? AND strftime('%Y', createdAt) = ?;`)).
+		WithArgs(
+			1,
+			2024,
+		).WillReturnRows(itemMockRows)
+
+	for _, tc := range testcases {
+		t.Run(tc.name, func(t *testing.T) {
+			i, err := tc.ir.GetMonthlyItems(1, 2024)
+			if err != tc.expectedErr {
+				t.Errorf("unexpected error\n want: %s\nhave: %s\n", tc.expectedErr, err.Error())
+			}
+
+			iBytes, err := json.Marshal(i)
+			if err != nil {
+				t.Error(err)
+			}
+
+			if tc.expectedResult != string(iBytes) {
+				t.Errorf("unexpected result \n want %s\nhave: %s\n", tc.expectedResult, string(iBytes))
 			}
 		})
 	}
